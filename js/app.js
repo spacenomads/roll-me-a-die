43113e7bd617fe51712b0c01dfc9d535bf387798
Carlos Mañas <carlos@spacenomads.com>
2021-05-09 09:25:23 +0200

    Validate roll format

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 const SEED = 'En serio?';
 const rollField = document.querySelector('#roll');
 const rollBtn = document.querySelector('#rollThis');
-
 const modifierCalc = {
   "+": (dice, modifier) => dice + modifier,
   "-": (dice, modifier) => dice - modifier,
@@ -10,22 +9,25 @@ const modifierCalc = {
   "/": (dice, modifier) => dice / modifier,
 };
 
+
+
+
+
 function getUserRoll(event) {
   const userRoll = rollField.value;
-
+  const isValid = !!userRoll.match(/[0-9][dD][0-9]+([\+\-\/\*][0-9]+?)/g);
   const tries = userRoll.split('d')[0];
-  const { dice, operation } = userRoll.match(/[dD](?<dice>[0-9]+)(?<operation>[\+-/*][0-9]+)/i).groups;
-    
-    console.group('Roll');
-    console.log({tries, dice, operation});
-    console.groupEnd();
-  
+  const { dice, modifier } = userRoll.match(/[dD](?<dice>[0-9]+)(?<modifier>[\+\-\/\*][0-9]+)?/i).groups;
+  console.log({isValid, tries, dice, modifier});
 }
+
+
+
 
 
 rollBtn.addEventListener('click', getUserRoll);
 
 
 Math.seedrandom(SEED, { entropy: true });
-console.log('> 0.9282578795792454', Math.random());          // Always 0.9282578795792454
-console.log('> 0.3752569768646784', Math.random());          // Always 0.3752569768646784
+//console.log('> 0.9282578795792454', Math.random());          // Always 0.9282578795792454
+//console.log('> 0.3752569768646784', Math.random());          // Always 0.3752569768646784
