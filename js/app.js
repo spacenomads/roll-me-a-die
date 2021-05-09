b22d75bd9f0065fa69b6c9f67011db9f5379d51a
Carlos Mañas <carlos@spacenomads.com>
2021-05-09 10:12:19 +0200

    Improve roll validator

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const SEED = 'En serio?';
 const rollField = document.querySelector('#roll');
 const rollBtn = document.querySelector('#rollThis');
+const ROLL_REGEXP = new RegExp(/[0-9][dD][0-9]+([\+\-\/\*][0-9]+)?/g);
+
 const modifierCalc = {
   "+": (dice, modifier) => dice + modifier,
   "-": (dice, modifier) => dice - modifier,
@@ -13,12 +15,25 @@ const modifierCalc = {
 
 
 
+function checkRollFormat(roll) {
+  return ROLL_REGEXP.test(roll);
+}
+
+
+
+
 function getUserRoll(event) {
   const userRoll = rollField.value;
-  const isValid = !!userRoll.match(/[0-9][dD][0-9]+([\+\-\/\*][0-9]+?)/g);
-  const tries = userRoll.split('d')[0];
-  const { dice, modifier } = userRoll.match(/[dD](?<dice>[0-9]+)(?<modifier>[\+\-\/\*][0-9]+)?/i).groups;
-  console.log({isValid, tries, dice, modifier});
+  const isValid = checkRollFormat(userRoll);
+
+  if (isValid) {
+
+    const tries = userRoll.split('d')[0];
+    const { dice, modifier } = userRoll.match(/[dD](?<dice>[0-9]+)(?<modifier>[\+\-\/\*][0-9]+)?/i).groups;
+    console.log({isValid, tries, dice, modifier});
+  } else {
+    console.log({userRoll, isValid});
+  }
 }
 
 
