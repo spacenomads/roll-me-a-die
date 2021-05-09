9e88d78694ddeaa00d2616bc54d8ddf7b5c3fb43
Carlos Mañas <carlos@spacenomads.com>
2021-05-09 17:29:54 +0200

    Improve matching regexp

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 const SEED = 'En serio?';
 const rollField = document.querySelector('#roll');
 const rollBtn = document.querySelector('#rollThis');
-const ROLL_REGEXP = new RegExp(/[0-9][dD][0-9]+([\+\-\/\*][0-9]+)?/g);
+const ROLL_REGEXP = new RegExp(/[0-9][d][0-9]+([\+\-\*\/][0-9]+)?/);
+
 
 const modifierCalc = {
   "+": (dice, modifier) => dice + modifier,
@@ -10,6 +11,7 @@ const modifierCalc = {
   "*": (dice, modifier) => dice * modifier,
   "/": (dice, modifier) => dice / modifier,
 };
+
 
 
 
@@ -23,16 +25,30 @@ function checkRollFormat(roll) {
 
 
 function getUserRoll(event) {
-  const userRoll = rollField.value;
+  const userRoll = rollField.value.trim().toLowerCase();
   const isValid = checkRollFormat(userRoll);
 
   if (isValid) {
 
     const tries = userRoll.split('d')[0];
-    const { dice, modifier } = userRoll.match(/[dD](?<dice>[0-9]+)(?<modifier>[\+\-\/\*][0-9]+)?/i).groups;
+    const { dice, modifier } = userRoll.match(/[d](?<dice>[0-9]+)(?<modifier>[\+\-\/\*][0-9]+)?/i).groups;
     console.log({isValid, tries, dice, modifier});
+    result.innerHTML = `
+      <p>tirada: ${userRoll}</p>
+      <p>Válida: Sí</p>
+      <p>nº de dados: ${tries}</p>
+      <p>caras: ${dice}</p>
+      <p>modificador: ${modifier || '--'}</p>
+    `;
   } else {
     console.log({userRoll, isValid});
+    result.innerHTML = `
+      <p>tirada: ${userRoll}</p>
+      <p>Válida: NO</p>
+      <p>nº de dados: --</p>
+      <p>caras: --</p>
+      <p>modificador: --</p>
+    `;
   }
 }
 
@@ -42,7 +58,32 @@ function getUserRoll(event) {
 
 rollBtn.addEventListener('click', getUserRoll);
 
+const rolls = [
+  "3d6*6",
+  "1d100",
+  "paco",
+  "2d6/2",
+  "2d6/2",
+  "2d6/2",
+  "3d6*3",
+  "2d5+10"
+]
 
-Math.seedrandom(SEED, { entropy: true });
+
+rolls.forEach(roll => {
+  const isValid = checkRollFormat(roll);
+  console.log(`tirada: ${roll} | Válida: ${isValid ? 'Sí' : 'No'}`);
+});
+
+//Math.seedrandom(SEED, { entropy: true });
 //console.log('> 0.9282578795792454', Math.random());          // Always 0.9282578795792454
 //console.log('> 0.3752569768646784', Math.random());          // Always 0.3752569768646784
+
+
+
+
+
+
+
+
+
